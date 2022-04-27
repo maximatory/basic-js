95a8a4654a4a0d3354532549fc72f72b564d579c
Maksim Baranov <oooinfo@mail.ru>
2022-04-27 20:40:11 +0300

    added carbon dating, dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
+  if (Number(sampleActivity) > 0 && typeof sampleActivity == 'string') {
+    let result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+    if (result > 0) {
+      return result;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = ""
+   if (Array.isArray(members) == true) {
+      let stringFilter = members.filter(function(elem) {
+         if (typeof elem == "string") {
+           return true
+         } else {
+           return false
+         }
+       })
+  let arr = stringFilter.map(function (elem) {
+    return elem.replace(/\s/g, '');
+  })
+      for (let elem of arr.sort(function (a, b) {
+         return a.toLowerCase().localeCompare(b.toLowerCase());
+  })) {
+    if (typeof elem == 'string') {
+      result = result + elem[0].toUpperCase();
+    } else {
+      result = false;
+    }
+  }
+} else {
+  result = false;
 }
+   return result;
+}
+
 
 module.exports = {
   createDreamTeam

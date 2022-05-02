34b5c625b1bd1c60a554997c021c799275fe8dc8
Maksim Baranov <oooinfo@mail.ru>
2022-05-02 22:10:23 +0300

    added mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -3,11 +3,11 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
+ * in the neighboring cells. Starting off with some matrixangement of mines
  * we want to create a Minesweeper game setup.
  *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
+ * @param {matrixay<matrixay>} matrix
+ * @return {matrixay<matrixay>}
  *
  * @example
  * matrix = [
@@ -23,9 +23,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper(matrix) {
+
+  let copy = JSON.parse(JSON.stringify(matrix))
+  for (let i = 0; i < copy.length; i++) {
+    for (let j = 0; j < copy[i].length; j++) {
+      copy[i][j] = 0
+    }
+  }
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        let sum = 0;
+        if (j < matrix[i].length - 1 && matrix[i][j + 1] === true) {
+          sum++
+        }
+        if (j > 0 && matrix[i][j - 1] === true) {
+          sum++
+        }
+        if (i < matrix.length - 1 && matrix[i + 1][j] === true) {
+          sum++
+        }
+        if (i > 0 && matrix[i - 1][j] === true) {
+          sum++
+        }
+        if (i < matrix.length - 1 && j < matrix[i].length - 1 && matrix[i + 1][j + 1] === true) {
+          sum++
+        }
+        if (i < matrix.length - 1 && j >0 && matrix[i + 1][j - 1] === true) {
+          sum++
+        }
+        if (i > 0 && j < matrix[i].length - 1 && matrix[i - 1][j + 1] === true) {
+          sum++
+        }
+        if (i > 0 && j > 0 && matrix[i - 1][j - 1] === true) {
+          sum++
+        }
+
+        copy[i][j] = sum
+      }
+    }
+    return copy
 }
 
 module.exports = {
